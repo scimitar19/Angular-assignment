50ee01d3aec7290a68fdab0f805c60be7d7d89b7
awais-iftikhar <awaissunny00@gmail.com>
2021-12-14 22:37:00 +0500

    landing page ui

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import Styles from './navbar.module.scss';
-
+import Logo from '../../assets/images/home/siteLogo.svg';
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import Box from "../Box";
+
 const Menu : React.FC = () => {
 
     return (
         <>
         <Navbar>
           <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home" className="d-flex align-items-center">
+              <img src={Logo} className="me-2" alt="" />
+              <Box className={Styles.textColor}>
+                 Verifiable Education
+              </Box>
+            </Navbar.Brand>
           <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">About Us</Nav.Link>
-            <Nav.Link href="#pricing">Verify</Nav.Link>
-            <Button variant="light" className={`${Styles.btnBorder} me-2`}>Sign in</Button> 
+            <Nav.Link href="#home" className={Styles.navLink}>Home</Nav.Link>
+            <Nav.Link href="#features" className={Styles.navLink}>About Us</Nav.Link>
+            <Nav.Link href="#pricing" className={`${Styles.textColor} me-2`}>Verify</Nav.Link>
+            <Button variant="light" className={`${Styles.btnBorder} ${Styles.navLink} me-2`}>Sign in</Button> 
             <Button variant="dark">Start Free Trial</Button>
           </Nav>
           </Container>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,29 @@
 import React from "react";
+import { Button, Container,Form} from "react-bootstrap";
 import Banner from '../../assets/images/home/cover.svg';
 import Box from "../../components/Box";
+
 import Styles from "./home.module.scss";
 const Home : React.FC = () => {
 
     return (
         <Box className={Styles.bgLight}>
+                  <Container>
                   <Box className={`${Styles.BannerContainer} w-50`}>
                             <Box className={Styles.BannerInnerContent} >
-                            <Box className={Styles.BannerHeading}>
-                            Make your students' achievements last forever
-                            </Box>
-                            <Box className={Styles.BannerText}>
-                            Add blockchain verifiable certificates to your offer.                        
-                            </Box>
+                                <Box className={Styles.BannerHeading}>
+                                    Make your students' achievements last forever
+                                </Box>
+                                <Box className={`${Styles.BannerText}`}>
+                                    Add blockchain verifiable certificates to your offer.                        
+                                </Box>
+                                <Box className="d-flex mt-3">
+                                <Form.Control type="email" placeholder="Email Address" className="w-50 me-4" />
+                                <Button  className={`${Styles.updateBtn} w-20`}>Get Updates</Button>
+                                </Box>
                             </Box >
                     </Box>
+                  </Container>
                     <Box className="w-50">
                         <img src={Banner} className={Styles.BannerImg} alt=""/>
                     </Box>

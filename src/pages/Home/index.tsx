90dfda87ac622cff9cabb7e8141740db8ee331a8
awais-iftikhar <awaissunny00@gmail.com>
2021-12-14 17:16:10 +0500

    ui design

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
 import Banner from '../../assets/images/home/cover.svg';
 import Box from "../../components/Box";
 import Styles from "./home.module.scss";

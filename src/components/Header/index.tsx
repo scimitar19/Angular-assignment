ffd76517df97cfb794e186088d915671fab40f52
awais-iftikhar <awaissunny00@gmail.com>
2021-12-18 02:57:40 +0500

    fixed responsixe issues and add icons and logout button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -19,9 +19,16 @@ const Header: React.FC<Props> = (props) => {
         variant="outline-info"
         onClick={props?.toggle}
         className={`${Style.expandIcon}`}
-      ></Button>
+      >
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M4 6H20" stroke="#048F46" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M4 12H20" stroke="#048F46" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M4 18H20" stroke="#048F46" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+
+      </Button>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav" className = "justify-content-between">
+      <Navbar.Collapse id="responsive-navbar-nav" className = {`justify-content-between`}>
         <Box className = {`${Style.searchMainDiv}`} >
           <input type="text" placeholder="Search" className = {`${Style.searchFiled}`} />
           <img src={SearchIcon} alt="" className = {`${Style.searchImage}`} />

237210f75e5b69ec959a9c67056401b2088703fb
Sulejman Sarajlija <3107367+Sulejman@users.noreply.github.com>
2021-12-17 16:42:12 +0100

    Update index.tsx

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,50 +1,50 @@
 import React from "react";
 import Styles from './navbar.module.scss';
 import Logo from '../../assets/images/home/siteLogo.svg';
-import { Navbar, Container, Nav, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import {Navbar, Container, Nav, Button} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 import Box from "../Box";
 
-const Menu : React.FC = () => {
+const Menu: React.FC = () => {
 
     return (
         <>
-        <Navbar>
-          <Container>
-          <Navbar.Brand  className="d-flex align-items-center">
-              <img src={Logo} className="me-2" alt="" />
-              <Box className={Styles.textColor}>
-                 Verifiable Education
-              </Box>
-            </Navbar.Brand>
-          <Nav className="ml-auto">
-            <Nav.Link  className={Styles.navLink}>
-              <Link to='/dashboard' className={Styles.link}>
-                  Home
-                </Link>
-            </Nav.Link>
-            <Nav.Link   className={Styles.navLink}>About Us</Nav.Link>
-            <Nav.Link  className={`${Styles.navLink} me-2`}>
-               <Link  to='/verify' className={Styles.link}> 
-                  Verify 
-                </Link>
-            </Nav.Link>
-            
-              <Link to='/auth/Login'>
-                <Button variant="light" className={`${Styles.btnBorder} ${Styles.navLink} me-2`}>
-                  Sign in
-                </Button>
-              </Link>
-              <Link to='auth/admin/singup' className={Styles.navLink}> 
-              <Button variant='dark'>
-                Start Free Trial
-              </Button>
-              </Link>
-          </Nav>
-          </Container>
-        </Navbar>
-      </>
+            <Navbar>
+                <Container>
+                    <Navbar.Brand className="d-flex align-items-center">
+                        <img src={Logo} className="me-2" alt=""/>
+                        {/*<Box className={Styles.textColor}>*/}
+                        {/*   Verifiable Education*/}
+                        {/*</Box>*/}
+                    </Navbar.Brand>
+                    <Nav className="ml-auto">
+                        <Nav.Link className={Styles.navLink}>
+                            <Link to='/dashboard' className={Styles.link}>
+                                Home
+                            </Link>
+                        </Nav.Link>
+                        <Nav.Link className={Styles.navLink}>About Us</Nav.Link>
+                        <Nav.Link className={`${Styles.navLink} me-2`}>
+                            <Link to='/verify' className={Styles.link}>
+                                Verify
+                            </Link>
+                        </Nav.Link>
+
+                        <Link to='/auth/Login'>
+                            <Button variant="light" className={`${Styles.btnBorder} ${Styles.navLink} me-2`}>
+                                Sign in
+                            </Button>
+                        </Link>
+                        <Link to='auth/admin/singup' className={Styles.navLink}>
+                            <Button variant='dark'>
+                                Start Free Trial
+                            </Button>
+                        </Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
     )
 }
 
